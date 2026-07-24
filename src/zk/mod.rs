c9155319b9903a3a1262f25b3a7621536d615f62
ing-eoking <koo05131@yahoo.co.kr>
2026-07-24 16:40:08 +0900

    INTERNAL: Implement recursive and stat ls for ZK mode

--- src/zk/mod.rs
+++ src/zk/mod.rs
@@ -72,6 +72,14 @@ fn parse_ls(args: &[&str]) -> ZkCommand {
     }
 }
 
+fn join_zk_path(parent: &str, child: &str) -> String {
+    if parent == "/" {
+        format!("/{}", child)
+    } else {
+        format!("{}/{}", parent, child)
+    }
+}
+
 struct ZkClient {
     zk: ZooKeeper,
 }
@@ -85,16 +93,55 @@ impl ZkClient {
         Ok(ZkClient { zk })
     }
 
+    fn print_stat(&self, stat: &zookeeper::Stat) {
+        println!("czxid: {}", stat.czxid);
+        println!("mzxid: {}", stat.mzxid);
+        println!("ctime: {}", stat.ctime);
+        println!("mtime: {}", stat.mtime);
+        println!("version: {}", stat.version);
+        println!("cversion: {}", stat.cversion);
+        println!("aversion: {}", stat.aversion);
+        println!("ephemeralOwner: {}", stat.ephemeral_owner);
+        println!("dataLength: {}", stat.data_length);
+        println!("numChildren: {}", stat.num_children);
+        println!("pzxid: {}", stat.pzxid);
+    }
+
+    fn ls_recursive(&self, path: &str) {
+        println!("{}", path);
+        match self.zk.get_children(path, false) {
+            Ok(children) => {
+                for child in children {
+                    self.ls_recursive(&join_zk_path(path, &child));
+                }
+            }
+            Err(e) => eprintln!("ERROR: {}: {:?}", path, e),
+        }
+    }
+
     /// Runs one command. Returns true when the loop should exit.
     fn execute(&self, cmd: ZkCommand) -> bool {
         match cmd {
             ZkCommand::Quit => return true,
             ZkCommand::Empty => {}
             ZkCommand::Unknown(msg) => eprintln!("ERROR: {}", msg),
-            ZkCommand::Ls { path, recursive: _, stat: _ } => match self.zk.get_children(&path, false) {
-                Ok(children) => println!("[{}]", children.join(", ")),
-                Err(e) => eprintln!("ERROR: {:?}", e),
-            },
+            ZkCommand::Ls { path, recursive, stat } => {
+                if recursive {
+                    self.ls_recursive(&path);
+                } else {
+                    match self.zk.get_children(&path, false) {
+                        Ok(children) => println!("[{}]", children.join(", ")),
+                        Err(e) => eprintln!("ERROR: {:?}", e),
+                    }
+                }
+                if stat {
+                    match self.zk.exists(&path, false) {
+                        Ok(Some(s)) => self.print_stat(&s),
+                        Ok(None) => eprintln!("ERROR: no such node: {}", path),
+                        Err(e) => eprintln!("ERROR: {:?}", e),
+                    }
+                }
+            }
             ZkCommand::Get(path) => match self.zk.get_data(&path, false) {
                 Ok((data, _stat)) => println!("{}", String::from_utf8_lossy(&data)),
                 Err(e) => eprintln!("ERROR: {:?}", e),
@@ -114,19 +161,7 @@ impl ZkClient {
                 Err(e) => eprintln!("ERROR: {:?}", e),
             },
             ZkCommand::Stat(path) => match self.zk.exists(&path, false) {
-                Ok(Some(stat)) => {
-                    println!("czxid: {}", stat.czxid);
-                    println!("mzxid: {}", stat.mzxid);
-                    println!("ctime: {}", stat.ctime);
-                    println!("mtime: {}", stat.mtime);
-                    println!("version: {}", stat.version);
-                    println!("cversion: {}", stat.cversion);
-                    println!("aversion: {}", stat.aversion);
-                    println!("ephemeralOwner: {}", stat.ephemeral_owner);
-                    println!("dataLength: {}", stat.data_length);
-                    println!("numChildren: {}", stat.num_children);
-                    println!("pzxid: {}", stat.pzxid);
-                }
+                Ok(Some(stat)) => self.print_stat(&stat),
                 Ok(None) => eprintln!("ERROR: no such node: {}", path),
                 Err(e) => eprintln!("ERROR: {:?}", e),
             },
@@ -274,5 +309,12 @@ mod tests {
     fn quit_and_unknown() {
         assert!(matches!(parse("quit"), ZkCommand::Quit));
         assert!(matches!(parse("frobnicate /a"), ZkCommand::Unknown(_)));
+    }
+
+    #[test]
+    fn join_zk_path_root_and_nested() {
+        assert_eq!(join_zk_path("/", "arcus"), "/arcus");
+        assert_eq!(join_zk_path("/arcus", "cache_list"), "/arcus/cache_list");
+        assert_eq!(join_zk_path("/a/b", "c"), "/a/b/c");
     }
 }
